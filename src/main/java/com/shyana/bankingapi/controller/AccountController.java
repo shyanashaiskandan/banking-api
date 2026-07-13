99243ba9d3bd696083effd3eedeb5d878d545bc9
SHYANA SHAISKANDAN <sshaiska@uwaterloo.ca>
2026-07-13 15:33:46 -0400

    account creation endpoint

---
+++ src/main/java/com/shyana/bankingapi/controller/AccountController.java
@@ -0,0 +1,24 @@
+package com.shyana.bankingapi.controller;
+
+import com.shyana.bankingapi.dto.AccountResponse;
+import com.shyana.bankingapi.dto.CreateAccountRequest;
+import com.shyana.bankingapi.service.AccountService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public AccountResponse createAccount(
+            @Valid @RequestBody CreateAccountRequest request) {
+        return accountService.createAccount(request);
+    }
+}
